b54dfc7d0a03847fd1c2023232252cd618bea458
jwkuang <254014188@qq.com>
2016-03-02 20:07:30 +0800

    查询、更新接口

--- server/DBUtils.py
+++ server/DBUtils.py
@@ -1,26 +1,53 @@
+# coding: utf-8
 __author__ = 'jwkuang'
-
 import pymongo
-
+from bson.objectid import ObjectId
 TYPE_TEACHER = 1
-TYPE_STUDENT = 2
+TYPE_HOMEWORK = 2
 
+REQUEST_STUDENT = 101
+REQUEST_TEACHER = 102
 
 mongoClient = pymongo.MongoClient('localhost', 27017)
 db = mongoClient.jwkuang
 collection = db.contentCollection
 
-def insertContent(student, teacher, title, contenttype, description, url):
+
+def insertContent(student, teacher, title, contenttype, description, contenturl, grade, homeworktype, homeworkurl):
     post = {"student": student,
             "teacher": teacher,
             "title": title,
             "type": contenttype,
             "description": description,
-            "url": url}
-    posts = collection.posts
-    posts.insert_one(post)
+            "url": contenturl,
+            "grade": grade,
+            "homeworktype": homeworktype,
+            "homeworkurl": homeworkurl}
+    contentid = collection.insert_one(post).inserted_id
+    return str(contentid)
+
+
+def getContentList(requesttype, student, teacher):
+    queryresult = []
+    if requesttype == REQUEST_STUDENT:
+        queryresult = collection.find({"$or": [{"student": student, "type": TYPE_HOMEWORK}, {"type": TYPE_TEACHER, "teacher": teacher}]})
+    elif requesttype == REQUEST_TEACHER:
+        queryresult = collection.find({"teacher": teacher})
+    return queryresult
+
+
+def updateContent(objectId, updateList):
+    collection.update_one({"_id": ObjectId(objectId)},
+                          {"$set": updateList})
     return
 
-def getContentList(contenttype, student, teacher):
-    
-    return
+
+if __name__ == '__main__':
+    contentid = insertContent("", "甜甜", "测试测试2", TYPE_TEACHER, "测试测试2", "www.baidu.com", 0, 0, "")
+    print collection.find_one({"_id": ObjectId(contentid)})
+    # result = getContentList(TYPE_HOMEWORK, "jwkuang", "甜甜")
+    # for content in result:
+    #     print content['description']
+    updateList = {"student": "jwkuang"}
+    updateContent(contentid, updateList)
+    print collection.find_one({"_id": ObjectId(contentid)})
